a59fbd8065e1c70fbae41c81acb9a1d3e6adfc38
Khary Mallea <kharymallea@gmail.com>
2016-06-07 21:45:26 -0400

    updated css

--- index.php
+++ index.php
@@ -24,6 +24,15 @@
       margin:0 auto 0;
     }
 
+    .post .col{
+      text-align: center;
+      text-align: -webkit-center;
+    }
+
+    .post .col:nth-child(even) {
+        background: #FBFBFB;
+    }
+
     .video-container{
       margin-bottom:20px;
     }
@@ -218,7 +227,7 @@
         ?>
         </div>
         <div class="col s12 m6">
-            <a class="twitter-timeline" href="https://twitter.com/Claption" data-widget-id="704865762322862080">Tweets by @Claption</a>
+            <a class="twitter-timeline" href="https://twitter.com/Claption" data-height="600" data-widget-id="704865762322862080">Tweets by @Claption</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
           </div>
       </div>
